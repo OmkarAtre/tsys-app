840b801a9bb2c07450fc7f16661406a41cd7efb5
OmkarAtre <96344629+OmkarAtre@users.noreply.github.com>
2022-05-17 10:36:12 +0530

    creating new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent,
+    UserListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
